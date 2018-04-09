c79827b13c0da6b573ef8aa4c161004e51e81895
Luciana <lprimante@gmail.com>
2018-04-09 16:53:22 -0300

    Aula de JS

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// let buttonNo = document.getElementById('btnNo');
+// let buttonYes = document.getElementById('btnYes');
+// const resultado = new Array ();
+
+// buttonNo.addEventListener('click') function() {
+//     for (var i = 0; i < resultado.length; i++) { 
+//         resultado[i];
+//         console.log('Ok');
+//     }
+// }
+
+// buttonYes.addEventListener('click') function() {
+//     for (var i = 0; i < resultado.length; i++) { 
+//         resultado[i];
+//         console.log('Ok');
+//     }
+// }
+
+
+
