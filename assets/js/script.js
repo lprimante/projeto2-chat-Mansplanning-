e6f0a8f3ff3698ab7d1d3b0a2c0a30c489735915
Luciana <lprimante@gmail.com>
2018-04-10 17:24:57 -0300

    Atualização da aula de JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,62 @@
 // let buttonNo = document.getElementById('btnNo');
 // let buttonYes = document.getElementById('btnYes');
-// const resultado = new Array ();
+// let no = 0;
+// let yes = 0;
+// const resultado = [];
 
-// buttonNo.addEventListener('click') function() {
+
+// let questionario  =  function() {
 //     for (var i = 0; i < resultado.length; i++) { 
-//         resultado[i];
-//         console.log('Ok');
-//     }
+//     resultado[i];
+
+//         if(buttonNo.addEventListener('click')) {
+//             } function no() {
+//                 console.log(no);
+//             }
+//         }
+
+//         if(buttonYes.addEventListener('click')) {
+//             } function yes() {
+//                 console.log(yes);
+//             }
 // }
 
-// buttonYes.addEventListener('click') function() {
-//     for (var i = 0; i < resultado.length; i++) { 
-//         resultado[i];
-//         console.log('Ok');
-//     }
-// }
+//----------------------------------------------------------
+
+// const resultado = [];
+
+// let questionario = function (resposta){
+//     for(var i = 0; i < resultado.length; i++)
+//     resultado = resposta[i];
+//     console.log(resultado);
+//   }
+
+//-----------------------------------------------------------
+
+let bancoDeImagens = ["01-sim.png", "02-sim.png", "03-sim.png", "04-sim.png", "05-nao.png", "06-nao.png", "07-sim.png", "08-sim.png", "09-nao.png", "10-sim.png"];
+
+let btnNo = document.getElementById('btnNo');
+let btnYes = document.getElementById('btnYes');
+const resultado = ["Sim", "Sim", "Sim", "Sim", "Não", "Não", "Sim", "Sim", "Não", "Sim"];
+const resposta = [];
+
+let contador = function(valor) {
+    for (var i = 1; i <= 10; i++) { 
+        if(i < 10) {
+            document.getElementById("imgTweet").src = "../assets/img/"+bancoDeImagens[i];
+            resposta[i] = "btn"+valor;
+            console.log(contador);
+            console.log(i);
+        } else {
+            window.location.replace("resultado.html")
+        }      
+    }
+}
+
+document.getElementById("btnNo").addEventListener('click', contador("No"));
+    
+document.getElementById("btnYes").addEventListener('click', contador("Yes"));
 
 
 
+    
