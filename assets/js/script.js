5a5130e37bdadfcb41f1b26d426b8402c36d7ce0
Luciana <lprimante@gmail.com>
2018-04-11 12:51:14 -0300

    Primeira parte do projeto concluída

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,26 +6,50 @@ const resultadoFinal = []; //Armazena as infos validadas com as repostas correta
 
 var i = 0;
 let contador = function(valor) {
-        if(i < 9) {
-            resposta[i] = valor; //Pega o valor do parametro no html do index, para deifinir qual botão foi clicado
-            resultadoFinal[i] = resposta[i] === resultado[i] ? true : false; //Valida se a resposta dada está correta ou errada
-            console.log(resposta[i], bancoDeImagens[i], resultadoFinal[i]); 
-            i++;      
-            document.getElementById("imgTweet").src = "./assets/img/"+bancoDeImagens[i]; //Troca a imagem ao clicar no botão     
-            sessionStorage.setItem("questionario", JSON.stringify(resultadoFinal)); //Armazena as informações do jogo para exibir na tela do resultado
+    resposta[i] = valor; //Pega o valor do parametro no html do index, para deifinir qual botão foi clicado
+        if(resposta[i] === resultado[i]) {
+            resultadoFinal[i] = true; //Valida se a resposta dada está correta ou errada
         } else {
+            resultadoFinal[i] = false; 
+        }    
+
+        i++;
+
+        if(i === resultado.length){               
+            sessionStorage.setItem("questionario", JSON.stringify(resultadoFinal)); //Armazena as informações do jogo para exibir na tela do resultado
             window.location.replace("resultado.html") //Redireciona para a página de resultados ao final do quiz
+        } else {
+            document.getElementById("imgTweet").src = "./assets/img/"+bancoDeImagens[i]; //Troca a imagem ao clicar no botão
         }      
 }
 
-
 let recuperar = JSON.parse(sessionStorage.getItem("questionario")); //Converte o dado da LocalStorage de string para array
 console.log(recuperar); 
+
 //Função ainda não funciona completamente
-i = 0; 
-do {
-        recuperar[i] === true ? "Acertou" : "Errou"; //Verifica se acertou ou errou a questão para printar na tela final do quiz
-        console.log(recuperar[i]);
-        document.getElementById("acertos").innerHTML = "<p><span>" +(i+1)+ "</span>" +recuperar[i]; //Descobrir como colocar linha no html 
-        i++;
-    } while (i < 9);
+
+i = 0;
+let counter = 0;
+let pAcertos = document.getElementById("resultado");
+
+console.log(pAcertos);
+
+let finalQuiz = function() {
+    for(i = 0; i < recuperar.length; i++) {
+        if(recuperar[i] === true) {
+            console.log(recuperar[i]);
+            document.getElementsByClassName("resultadoFinal")[i].innerHTML = "<span>" +(i+1)+ "</span> ACERTOU";
+            //document.getElementsByClassName("span")[i].style.backgroundColor = "green";
+            counter += 1;
+            counter <= 1 ? pAcertos.innerHTML = counter+ " ACERTO" : pAcertos.innerHTML = counter+ " ACERTOS";
+        } else {
+            document.getElementsByClassName("resultadoFinal")[i].innerHTML = "<span>" +(i+1)+ "</span> ERROU";
+            //document.getElementsByClassName("span")[i].style.backgroundColor = "red";
+        } console.log(); 
+    }
+}
+
+sessionStorage.getItem(finalQuiz());
+
+
+
